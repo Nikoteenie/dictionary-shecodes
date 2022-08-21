0ed977c6988900e32c4e5802fd22b76477fbc091
Nikoteenie <105375963+Nikoteenie@users.noreply.github.com>
2022-08-21 14:46:40 -0400

    Added Search Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./yello.gif";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-     
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+          </main>
+        <footer className="App-footer">Coded by Nicole Brodkin</footer>
+      </div>
     </div>
   );
 }
